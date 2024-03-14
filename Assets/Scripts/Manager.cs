3e1d27d1b5f6713a39a6647c49ba2e22b5e1c68c
server-123 <kyumintv63@gmail.com>
2024-03-14 20:40:49 +0900

    Setting

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -7,34 +7,11 @@ public class Manager : MonoBehaviour
     void Start()
     {
         chz = GetComponent<ChzzkChat>();
-        SetResolution();
     }
 
     public void Quit()
     {
         chz.Disconncect();
         Application.Quit();
-    }
-
-    public void SetResolution()
-    {
-        int setWidth = 1920;
-        int setHeight = 1080;
-
-        int deviceWidth = Screen.width;
-        int deviceHeight = Screen.height;
-
-        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true);
-
-        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight)
-        {
-            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight);
-            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f);
-        }
-        else
-        {
-            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight);
-            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight);
-        }
     }
 }
